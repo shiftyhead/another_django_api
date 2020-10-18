ac72d5f639abccd7b3255e468f0b2656db6d3436
spirin <spirin@smartsolutions-123.ru>
2020-10-18 18:55:46 +0400

    Add tests

--- book_api/users/models.py
+++ book_api/users/models.py
@@ -6,8 +6,12 @@ class Account(models.Model):
     name = models.CharField(
         max_length=50
     )
-    birthday = models.DateField()
-    subscription_end = models.DateField()
+    birthday = models.DateField(
+        default=date(year=1970, month=1, day=1)
+    )
+    subscription_end = models.DateField(
+        default=date.today() + timedelta(weeks=2)
+    )
 
     def subscription_is_active(self):
         return self.subscription_end >= date.today()
--- book_api/users/tests.py
+++ book_api/users/tests.py
@@ -1,3 +1,27 @@
+import json
+from collections import namedtuple
+
+from datetime import date, timedelta
 from django.test import TestCase
 
-# Create your tests here.
+from .views import create_user, get_user_detail
+
+
+class AccountTestCase(TestCase):
+    def setUp(self):
+        Request = namedtuple('Request', ['method', 'body'])
+        request = Request('POST', '{"name": "Maria", "birthday": "2002-10-30"}')
+        create_user(request)
+        request = Request('POST', '{"name": "Cheater", "birthday": "2002-10-30", "subscription_end": "3002-10-30"}')
+        create_user(request)
+
+    def test_animals_can_speak(self):
+        correct_trial = date.today() + timedelta(weeks=2)
+
+        maria = get_user_detail('', 1)
+        content = json.loads(maria.content.decode())
+        self.assertEqual(content.get('subscription_end'), str(correct_trial))
+
+        cheater = get_user_detail('', 2)
+        content = json.loads(cheater.content.decode())
+        self.assertEqual(content.get('subscription_end'), str(correct_trial))
