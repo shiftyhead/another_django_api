577484399fc7d2120a502179c1a2776bb81e44ce
spirin <spirin@smartsolutions-123.ru>
2020-10-23 12:18:53 +0400

    Account creation test reformat

--- book_api/users/tests.py
+++ book_api/users/tests.py
@@ -8,19 +8,20 @@ from .views import create_user, get_user_detail
 
 
 class AccountTestCase(TestCase):
-    def setUp(self):
+
+    def test_account_creation(self):
         Request = namedtuple('Request', ['method', 'body'])
+        correct_trial = date.today() + timedelta(weeks=2)
+
         request = Request('POST', '{"name": "Maria", "birthday": "2002-10-30"}')
         create_user(request)
-        request = Request('POST', '{"name": "Cheater", "birthday": "2002-10-30", "subscription_end": "3002-10-30"}')
-        create_user(request)
-
-    def test_animals_can_speak(self):
-        correct_trial = date.today() + timedelta(weeks=2)
 
         maria = get_user_detail('', 1)
         content = json.loads(maria.content.decode())
         self.assertEqual(content.get('subscription_end'), str(correct_trial))
+
+        request = Request('POST', '{"name": "Cheater", "birthday": "2002-10-30", "subscription_end": "3002-10-30"}')
+        create_user(request)
 
         cheater = get_user_detail('', 2)
         content = json.loads(cheater.content.decode())
