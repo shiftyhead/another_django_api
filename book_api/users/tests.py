f93183cbc57771d7cf64a04bf5815217f11b292b
spirin <spirin@smartsolutions-123.ru>
2020-10-25 22:59:02 +0400

    Add month pay test

--- book_api/users/tests.py
+++ book_api/users/tests.py
@@ -1,13 +1,15 @@
 from datetime import date, timedelta
 from django.test import TestCase, Client
 
+from .models import Account
+
 
 class AccountTestCase(TestCase):
     client = Client()
-    correct_trial = date.today() + timedelta(weeks=2)
+    correct_user_data = {'name': 'Maria', 'birthday': '2002-10-30'}
 
     def test_creation(self):
-        user_data = {'name': 'Maria', 'birthday': '2002-10-30'}
+        user_data = self.correct_user_data
         creation_response = self.client.post('/users/new/', user_data, content_type='application/json')
         self.assertEqual(creation_response.status_code, 200)
 
@@ -33,7 +35,7 @@ class AccountTestCase(TestCase):
 
     def test_user_subscription(self):
         users_data = [
-            {'name': 'Maria', 'birthday': '2002-10-30'},
+            self.correct_user_data,
             {'name': 'Cheater', 'birthday': '2000-12-20', 'subscription_end': '3000-12-31'},
         ]
         for user_data in users_data:
@@ -45,5 +47,28 @@ class AccountTestCase(TestCase):
             new_user_response_data = new_user_response.json()
             self.assertEqual(
                 new_user_response_data['subscription_end'],
-                str(self.correct_trial)
+                str(date.today() + timedelta(weeks=2))
             )
+
+    def test_pay_month_subscription(self):
+        creation_response = self.client.post('/users/new/', self.correct_user_data, content_type='application/json')
+        creation_response_data = creation_response.json()
+        new_user_id = creation_response_data['user_id']
+
+        new_user_entity = Account.objects.get(pk=new_user_id)
+        new_user_entity.subscription_end = date(2020, 12, 5)
+        new_user_entity.save()
+
+        pay_month = {'status': 'ok', 'period': 'month', 'user_id': new_user_id}
+        pay_response = self.client.post('/users/pay/', pay_month, content_type='application/json')
+        pay_response_data = pay_response.json()
+        self.assertEqual(pay_response_data['status'], 'success')
+
+        paid_user_response = self.client.get(f'/users/{new_user_id}/')
+        paid_user_response_data = paid_user_response.json()
+
+        month_pay_date = date.fromisoformat(paid_user_response_data['subscription_end'])
+        self.assertEqual(
+            month_pay_date,
+            date(2021, 1, 5)
+        )
